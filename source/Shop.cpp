08fe78157c7049e2430552ebb9bd525af2a6cb19
DarksoftwareLLC <41650368+DarksoftwareLLC@users.noreply.github.com>
2018-07-26 09:44:38 +0100

    Add files via upload

--- source/Shop.cpp
+++ source/Shop.cpp
@@ -21,7 +21,8 @@ int main()
 	printf("%s%s%s", CONSOLE_RED, "Please be aware by using this program you risk a ban", CONSOLE_RESET);
     printf("\n");
     printf("[eXhop:Info] Reading title file (sdmc:/switch/eXhop.txt)\n");
-    printf("[eXhop:Info] Press A to download the title from exhop.txt");
+    printf("[eXhop:Info] Press A to download the title from exhop.txt to the SD.\n");
+    printf("[eXhop:Info] Press B to download the title from exhop.txt to NAND.\n");
     while(appletMainLoop())
     {
         gfxFlushBuffers();
@@ -40,6 +41,23 @@ int main()
 					buf.pop_back();
 					u64 id = strtoull(buf.c_str(), NULL, 16);
 					nsBeginInstallApplication(id, 0, FsStorageId_SdCard);
+					printf("started downloading %s", id);
+				}
+			}
+			ifs.close();
+			printf("%s%s%s", CONSOLE_GREEN, "Complete, feel free to exit to the home menu.", CONSOLE_RESET);
+        }
+		if(k & KEY_B)
+        {
+			ifstream ifs("sdmc:/switch/eXhop.txt");
+			if(ifs.good())
+			{
+				string buf;
+				while(getline(ifs, buf))
+				{
+					buf.pop_back();
+					u64 id = strtoull(buf.c_str(), NULL, 16);
+					nsBeginInstallApplication(id, 0, FsStorageId_NandUser);
 					printf("started downloading %s", id);
 				}
 			}
